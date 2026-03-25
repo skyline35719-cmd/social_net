3dc7e5e12dde29ce70f73df8088f52021a8cdefc
skyrol <skyline35719@gmail.com>
2026-03-25 17:05:36 +0100

    add groups

--- social_net/posts/models.py
+++ social_net/posts/models.py
@@ -6,7 +6,15 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# Create your models here.
+
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название группы')
+    slug = models.SlugField(unique=True, verbose_name='Уникальный адрес')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -15,6 +23,12 @@ class Post(models.Model):
         on_delete=models.CASCADE, 
         related_name='posts'
         )
-    
-    
-    
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='posts'
+        )
+ 
+  
--- social_net/posts/views.py
+++ social_net/posts/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models  import Post, Group
+from django.contrib.auth import get_user_model
 
 
-# Create your views here.
-#def index(request):
-#    return HttpResponse('Main page')
+User = get_user_model()
 
 def index(request):
-    template = 'temp.html'
-    title = 'Anfisa for friends'
+    #posts = Post.objects.order_by('-pub_date')[:10]
+    #posts = [Post.objects.get(id=5)]
+    posts = Post.objects.filter(author_id=2)
+    users  = User.objects.all()
+    for user in users:
+        print(user.id)
+        print(user.username)
+    for post in posts:
+        print(post.text[:10])
+        
     context = {
-        'title': title,
-        'text' : 'Main page'
+        'posts':posts,
+        }
+    return render(request, 'posts/index1.html', context)
+
+
+def group_posts(request, slug):
+    # Получаем объект группы по slug или возвращаем 404 ошибку
+    group = get_object_or_404(Group, slug=slug)
+    
+    # Получаем все посты, принадлежащие этой группе
+    # Сортируем по дате публикации от новых к старым
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    
+    # Можно добавить ограничение на количество постов
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    
+    context = {
+        'group': group,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
